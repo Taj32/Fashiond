37407a373d80700130217129a579a4635bc687cf
Tajvir Chahal <tajvirchahal87@gmail.com>
2022-11-09 12:54:52 -0500

    Added gateway lock, and sign in button
    - added scroll lock while the user has not yet hit the sign in button

---
+++ js_files/scroll.js
@@ -0,0 +1,16 @@
+// File handles enabling and disabling scroll feature
+// will be used to lock the user to a gateway
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
